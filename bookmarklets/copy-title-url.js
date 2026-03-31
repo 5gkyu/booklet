edc2e5508bb44c02118b0bb5f20d2eea785005b8
kyu <155219899+5gkyu@users.noreply.github.com>
2026-04-01 02:55:09 +0900

    Delete bookmarklets directory

--- bookmarklets/copy-title-url.js
+++
@@ -1,9 +0,0 @@
-// タイトル＆URLコピー
-// 閲覧中のページタイトルとURLをクリップボードにコピーします。
-javascript:void(function(){
-  var t = document.title,
-      u = location.href;
-  navigator.clipboard.writeText(t + '\n' + u)
-    .then(function() { alert('コピーしました!\n' + t); })
-    .catch(function() { prompt('コピーしてください:', t + ' ' + u); });
-})()
